74e14d1a2d1fd0294f3a3731494c2dae5ae4e92b
brunoharlein <bruno@harlein.com>
2020-03-10 13:43:00 +0100

    form register

--- app/forms.py
+++ app/forms.py
@@ -25,3 +25,30 @@ class LoginForm(FlaskForm):
     password = PasswordField('Mot de passe', validators=[DataRequired()])
     submit = SubmitField('Se connecter')
 
+class RegisterForm(FlaskForm):
+    """Class to generate a form for the user to register on the site"""
+    """Classe pour générer un formulaire d'inscription de l'utilisateur sur le site"""
+    last_name = StringField('Nom', validators=[DataRequired()])
+    first_name = StringField('Prénom', validators=[DataRequired()])
+    pseudo = StringField('Pseudo', validators=[DataRequired()])
+    description = TextField('Description personnelle')
+    password = PasswordField('Mot de passe', validators=[
+        DataRequired(),
+        Regexp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}$", message="Attention le mot de passe doit contenir 6 caractères, une minuscule, une majuscule et un chiffre")
+    ])
+    password_confirm = PasswordField('Confirmez le mot de passe', validators=[
+        DataRequired(),
+        EqualTo('password', "Les deux mot de passes doivent être identiques")
+    ])
+    submit = SubmitField("S'inscrire")
+
+    # NOTE: this a special validation system from wtfform
+    # REMARQUE: ceci est un système de validation spécial de wtfform
+    # each method of a form starting with validate_ + the field name is called when submitting the form
+    # chaque méthode d'un formulaire commençant par validate_ + le nom du champ est appelée lors de la soumission du formulaire
+    def validate_pseudo(self, pseudo):
+        """Function that checks that the pseudo is not already used in database"""
+        """Fonction qui vérifie que le pseudo n'est pas déjà utilisé dans la base de données"""
+        user = User.query.filter_by(pseudo=pseudo.data).first()
+        if user is not None:
+            raise ValidationError('Ce pseudo est déjà pris :(')
